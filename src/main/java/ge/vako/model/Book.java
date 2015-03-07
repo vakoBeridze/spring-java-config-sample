9ec17cf02009c53b650ff0c52748434a71eb7b87
Vako Beridze <vako.beridze@gmail.com>
2015-03-07 21:42:16 +0400

    created project

---
+++ src/main/java/ge/vako/model/Book.java
@@ -0,0 +1,35 @@
+package ge.vako.model;
+
+/**
+ * Created by vako on 3/7/15.
+ */
+public class Book {
+
+    private String id;
+    private String email;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
